bd08339ecd4e1f2f1db3d6b06d555d18221f295b
Juan Zurita <81644288+JuanZurita96@users.noreply.github.com>
2023-03-20 19:11:02 -0600

    feat(components-logic): add components to handle logic and show information (#3)
    * Initial Commit: project structure
    
    * chore(project-root): clean default files
    
    * chore(project-structure): create redux state and tree components
    
    * feat(components-logic): add components to handle logic and show information
    
    ---------
    
    Co-authored-by: Juan Zurita

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,65 @@
 import React from 'react'
+import {
+  BrowserRouter as AppRouter,
+  Routes,
+  Route,
+  Navigate,
+  Link as RouterLink,
+  LinkProps as RouterLinkProps,
+} from 'react-router-dom'
+import {
+  Container,
+  BottomNavigation,
+  BottomNavigationAction,
+} from '@mui/material'
+import { Restore, Favorite } from '@mui/icons-material'
 import { TaskHistory, TaskList } from './Containers'
 
 function App() {
+  const [value, setValue] = React.useState('lista')
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) =>
+    setValue(newValue)
+
+  const LinkBehavior = React.forwardRef<
+    HTMLAnchorElement,
+    Omit<RouterLinkProps, 'to'> & { href: RouterLinkProps['to'] }
+  >((props, ref) => {
+    const { href, ...other } = props
+    return <RouterLink ref={ref} to={href} {...other} />
+  })
+
   return (
-    <div>
-      <TaskHistory />
-      <TaskList />
-    </div>
+    <Container>
+      <AppRouter>
+        <BottomNavigation
+          showLabels
+          sx={{ width: 250 }}
+          value={value}
+          onChange={handleChange}
+        >
+          <BottomNavigationAction
+            label="Lista"
+            value="lista"
+            icon={<Restore />}
+            href={'./'}
+            LinkComponent={LinkBehavior}
+          />
+          <BottomNavigationAction
+            label="Historial"
+            value="historial"
+            icon={<Favorite />}
+            href={'./history'}
+            LinkComponent={LinkBehavior}
+          />
+        </BottomNavigation>
+        <Routes>
+          <Route path="/history" element={<TaskHistory />} />
+          <Route path="/" element={<TaskList />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </AppRouter>
+    </Container>
   )
 }
 
--- src/State/taskReducer.ts
+++ src/State/taskReducer.ts
@@ -1,34 +1,105 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { Task } from '../Interfaces/Task'
 
-const findTask = (payload: Task) =>
-  initialState.findIndex((task) => task.id === payload.id)
+interface TaskState {
+  completedTasks: Task[]
+  activeTasks: Task[]
+}
 
-const initialState = [
-  {
-    id: 1,
-    status: 'completed',
-    title: 'Hacer la tarea',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    duration: 30,
-    creationDate: new Date().toLocaleString(),
-    completionTime: 25,
-  },
-]
+const generateTasks = (numberOfTasks: number) => {
+  const titles = [
+    'Comprar comida',
+    'Hacer ejercicio',
+    'Estudiar programación',
+    'Hacer la cama',
+    'Limpiar el baño',
+    'Salir al cine',
+    'Terminar el proyecto',
+  ]
+  const taskList = []
+
+  for (let i = 0; i < numberOfTasks; i++) {
+    const lastSevenDays = Math.floor(Math.random() * 7)
+    const date = new Date()
+    date.setDate(date.getDate() - lastSevenDays)
+    const timeLimit = Math.floor(Math.random() * 121)
+    const hours = Math.floor(timeLimit / 60)
+    const minutes = timeLimit % 60
+    const seconds = (timeLimit * 60) % 60
+    const visualTimeLimit = `${hours > 0 ? hours + ':' : '0:'}${minutes
+      .toString()
+      .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+
+    const getCompletionTime = (time: number) => {
+      const timeLimitPerTask = time * 60
+      const minValueToComplete = timeLimitPerTask * 0.8
+      return `${Math.floor(
+        (Math.random() * (timeLimitPerTask - minValueToComplete + 1) +
+          minValueToComplete) /
+          60
+      )} minutes`
+    }
+
+    const newTask = {
+      id: i + 1,
+      status: 'completed' as const,
+      title: titles[Math.floor(Math.random() * titles.length)],
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      duration: visualTimeLimit,
+      creationDate: date.toLocaleDateString(),
+      completionTime: getCompletionTime(timeLimit),
+    }
+    taskList.push(newTask)
+  }
+
+  return taskList
+}
+
+const findTask = (state: TaskState, id: number) =>
+  state.activeTasks.findIndex((task) => task.id === id)
+
+const initialState = {
+  completedTasks: generateTasks(50),
+  activeTasks: [],
+} as TaskState
 
 const tasksActions = createSlice({
   name: 'Task Actions',
   initialState,
   reducers: {
-    changeStatus: (state, action) => {
-      state[findTask(action.payload)].status = action.payload
+    addTask: (state, action) => {
+      state.activeTasks = [...state.activeTasks, action.payload]
     },
-    changeDescription: (state, action) => {
-      state[findTask(action.payload)].description = action.payload
+    updateTask: (state, action) => {
+      state.activeTasks[findTask(state, action.payload.id)] = action.payload
     },
-    changeDuration: (state, action) => {
-      state[findTask(action.payload)].duration = action.payload
+    changeState: (state, { payload }) => {
+      switch (payload.type) {
+        case 'start':
+        case 'paused':
+        case 'stopped':
+          state.activeTasks[findTask(state, payload.id)].status = payload.type
+          break
+        case 'restart':
+          state.activeTasks[findTask(state, payload.id)].timerLeft =
+            state.activeTasks[findTask(state, payload.id)].duration
+          break
+        case 'completed':
+          const [taskComplete] = state.activeTasks.splice(
+            findTask(state, payload.id),
+            1
+          )
+          state.completedTasks = [
+            ...state.completedTasks,
+            {
+              ...taskComplete,
+              id: state.completedTasks.length + 1,
+              status: 'completed',
+            },
+          ]
+          break
+      }
     },
   },
 })
